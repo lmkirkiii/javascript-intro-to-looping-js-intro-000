4000729bc22ac586b44f1f5dd95ba98570e4ad5a
lmkirkiii@gmail.com Kirk <lmkirkiii@gmail.com>
2018-04-24 02:05:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,1 +1,11 @@
-
+function forLoop(array){
+  for(let index = 0; index < 25; index++){
+    if(index === 1){
+      array.push(`"I am ${index} strange loop."`);
+    }
+    else{
+      array.push(`I am ${index} strange loops.`);
+    }
+  }
+  return array;
+}
